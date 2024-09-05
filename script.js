597ca3b2b0bc1330b0046f6bc3465a930dacf386
Javier <lessi111@icloud.com>
2024-09-05 12:56:12 -0400

    Add initial files, and bare calculator

---
+++ script.js
@@ -0,0 +1,32 @@
+let firstNum = "";
+let operator = "";
+let secondNum = "";
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(first, second, operator) {
+  switch (operator) {
+    case "+":
+      return add(first, second);
+    case "-":
+      return subtract(first, second);
+    case "*":
+      return multiply(first, second);
+    case "/":
+      return divide(first, second);
+  }
+}
