281da817f7adcf604af63717b0e5b5c31682efde
Javier <lessi111@icloud.com>
2024-09-07 08:59:49 -0400

    Add better logic, catch errores and final touches

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let nums = ["", ""];
 let operator = "";
 let currentNumber = 0;
+let result = 0;
 
 const clearButton = document.querySelector("#ac");
 const plusminus = document.querySelector("#plusminus");
@@ -18,22 +19,80 @@ const sumTwo = document.querySelector("#sum");
 const one = document.querySelector("#one");
 const two = document.querySelector("#two");
 const three = document.querySelector("#three");
+const zero = document.querySelector("#zero");
 const equal = document.querySelector("#equal");
+const display = document.querySelector("#display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+display.textContent = result;
 
-clearButton.addEventListener("click", () => {
+const refresh = () => {
+  display.textContent = `${nums[1] ? nums[0] : ""} ${
+    operator && nums[1] ? operator : ""
+  } ${!nums[1] ? nums[0] : nums[currentNumber]}`;
+};
+
+const clear = () => {
   nums = ["", ""];
   operator = "";
   currentNumber = 0;
+};
+
+const equals = () => {
+  if (nums[0] && nums[1]) {
+    if (operator === "sum") {
+      result = add(+nums[0], +nums[1]);
+    } else if (operator === "subtract") {
+      result = subtract(+nums[0], +nums[1]);
+    } else if (operator === "multiply") {
+      result = multiply(+nums[0], +nums[1]);
+    } else if (operator === "divide") {
+      if (nums[0] === "0" || nums[1] === "0") {
+        alert("I could barely make this calculator!!!");
+        clear();
+        result = null;
+      } else {
+        result = divide(+nums[0], +nums[1]);
+      }
+    } else if (operator === "modulo") {
+      result = modulo(+nums[0], +nums[1]);
+    }
+
+    if (Number.isInteger(result)) {
+      nums = [result.toString(), ""];
+    } else if (result === null) {
+      result = 0;
+      nums = ["", ""];
+    } else {
+      nums = [result.toFixed(1).toString(), ""];
+    }
+    display.textContent = result;
+  }
+};
+
+clearButton.addEventListener("click", () => {
+  clear();
+  display.textContent = "0";
 });
 
-equal.addEventListener("click", () => {
-  if (operator === "sum") {
-    alert(add(nums[0], nums[1]))
-  }
-})
+equal.addEventListener("click", equals);
+
+operators.forEach((item) => {
+  item.addEventListener("click", () => {
+    currentNumber = 1;
+    if (nums[0] && nums[1]) {
+      equals();
+    }
+  });
+});
 
 plusminus.addEventListener("click", () => {
-  nums[currentNumber] = (-parseInt(nums[currentNumber])).toString();
+  if (nums[1]) {
+    nums[1] = (-parseInt(nums[1])).toString();
+  } else {
+    nums[0] = (-parseInt(nums[0])).toString();
+  }
+  refresh();
 });
 
 div.addEventListener("click", () => {
@@ -52,40 +111,61 @@ sumTwo.addEventListener("click", () => {
   operator = "sum";
 });
 
+mod.addEventListener("click", () => {
+  operator = "modulo";
+});
+
+operators.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (!nums[0]) {
+      currentNumber = 0;
+      operator = "";
+    }
+  });
+});
+
 seven.addEventListener("click", () => {
-  nums[currentNumber] = "7";
+  nums[currentNumber] += "7";
 });
 
 eight.addEventListener("click", () => {
-  nums[currentNumber] = "8";
+  nums[currentNumber] += "8";
 });
 
 nine.addEventListener("click", () => {
-  nums[currentNumber] = "9";
+  nums[currentNumber] += "9";
 });
 
 four.addEventListener("click", () => {
-  nums[currentNumber] = "4";
+  nums[currentNumber] += "4";
 });
 
 five.addEventListener("click", () => {
-  nums[currentNumber] = "5";
+  nums[currentNumber] += "5";
 });
 
 six.addEventListener("click", () => {
-  nums[currentNumber] = "6";
+  nums[currentNumber] += "6";
 });
 
 one.addEventListener("click", () => {
-  nums[currentNumber] = "1";
+  nums[currentNumber] += "1";
 });
 
 two.addEventListener("click", () => {
-  nums[currentNumber] = "2";
+  nums[currentNumber] += "2";
 });
 
 three.addEventListener("click", () => {
-  nums[currentNumber] = "3";
+  nums[currentNumber] += "3";
+});
+
+zero.addEventListener("click", () => {
+  nums[currentNumber] += "0";
+});
+
+numbers.forEach((item) => {
+  item.addEventListener("click", refresh);
 });
 
 function add(a, b) {
@@ -102,6 +182,10 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
 }
 
 function operate(first, second, operator) {
